9e70e884e855f20f5b793ffea464d920fd23f1b5
Harshal-Girme <33945395+Harshal-Girme@users.noreply.github.com>
2018-04-15 22:25:51 -0400

    Add files via upload

--- Algorithm Project/src/knapsackProblem/InputUser.java
+++ Algorithm Project/src/knapsackProblem/InputUser.java
@@ -16,24 +16,15 @@ import static knapsackProblem.Knapsack.isInt;
 public class InputUser {
 
     
-    //Knapsack knapsack = new Knapsack();
+    
     public void getInput(Knapsack knapsack) {
 
-        // Hold user userInput, line by line
         String userInput;
 
-        // Initialize console for user userInput
-        //Console c = System.console();
         Scanner sc = new Scanner(System.in);
-//        if (c == null) {
-//            System.err.println("No console.");
-//            System.exit(1);
-//        }
 
-        // Number of items
         System.out.println("Enter total number of items for knapsack: ");
         userInput = sc.nextLine();
-        //userInput = c.readLine();
         
         if (isInt(userInput)) {
             knapsack.totalItems = Integer.parseInt(userInput);
@@ -43,7 +34,6 @@ public class InputUser {
             System.exit(1);
         }
 
-        // Value and weight of each item
         for(int i = 0; i < knapsack.totalItems; i++) {
             System.out.println("Please give input for Value of Item " + (i + 1) + ": ");
             userInput = sc.nextLine();
@@ -66,7 +56,6 @@ public class InputUser {
             }            
         }
 
-        // Capacity of knapsack
         System.out.println("Please enter total capacity of knapsack ");
         userInput = sc.nextLine();
         if (isInt(userInput)) {
@@ -77,7 +66,6 @@ public class InputUser {
             System.exit(1);
         }
 
-        // Population size
         System.out.println("Please enter total population size ");
         userInput = sc.nextLine();
         if (isInt(userInput)) {
@@ -88,7 +76,6 @@ public class InputUser {
             System.exit(1);
         }
 
-        // Maximum number of generations
         System.out.println("Please enter total number of Generations ");
         userInput = sc.nextLine();
         if (isInt(userInput)) {
@@ -99,7 +86,6 @@ public class InputUser {
             System.exit(1);
         }
 
-        // Crossover probability
         System.out.println("Please Enter prob for cross: ");
         userInput = sc.nextLine();
         if (isDoub(userInput)) {
@@ -110,7 +96,6 @@ public class InputUser {
             System.exit(1);
         }
 
-        // Mutation probability
         System.out.println("Please enter prob for mutation: ");
         userInput = sc.nextLine();
         if (isDoub(userInput)) {
